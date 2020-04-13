0c63c7a2da39d9ae2b1bc011c7dd23f5eb35677b
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2020-04-14 01:59:35 +0200

    handle backend routes with methods

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('hello'));
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions');
+
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransaction);
+
+router
+    .route('/:id')
+    .delete(deleteTransaction)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const transactions = require('./routes/transactions');
 
 const app = express();
 
-app.use('/', transactions);
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
